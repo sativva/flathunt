4a68c98f876bcf6b4fe332d034ad29e2830240f4
thomas rondio <sativva@free.fr>
2016-09-28 11:53:12 +0200

    add some credential postmake

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
-  address: ENV['7022e888-702e-4310-9300-44531b99ba82'],
+  address: ENV['POSTMARK_SMTP_SERVER'],
   port: '25',
-  domain: 'mylovelyplace.heroku.com',
+  domain: 'heroku.com',
   user_name: ENV['POSTMARK_API_KEY'],
   password: ENV['POSTMARK_API_TOKEN'],
   authentication: :cram_md5,

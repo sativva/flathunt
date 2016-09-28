a84baea8e32b8004dad739f8a0d1f7596513efbc
thomas rondio <sativva@free.fr>
2016-09-28 10:59:11 +0200

    try to configure actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,12 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "flathunt_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "mylovelyplace.herokuapp.com" }
+  # or your custom domain name eg. "www.yourdomain.com"
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false

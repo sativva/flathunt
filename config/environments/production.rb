54e415b189953b7d0a1e94ddafbaf1f096393a6e
thomas rondio <sativva@free.fr>
2016-10-05 12:26:07 +0200

    try to figure out how mailer works on heroku again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
 
   # config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "mylovelyplace.herokuapp.com" }
+  # config.action_mailer.default_url_options = { host: "mylovelyplace.herokuapp.com" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   # or your custom domain name eg. "www.yourdomain.com"

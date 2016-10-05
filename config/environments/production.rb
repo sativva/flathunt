ffe6e2aa8bf7cb3e70b0182800900b8f4f8674d0
thomas rondio <sativva@free.fr>
2016-10-05 16:05:38 +0200

    fix bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,17 +66,17 @@ Rails.application.configure do
   #
   #
 
-  # config.action_mailer.default_url_options = { :host => 'mylovelyplace.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'mylovelyplace.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
-    :authentication       => "plain"
-    # :enable_starttls_auto => true
+    :authentication       => "plain",
+    :enable_starttls_auto => true
     # address: "smtp.gmail.com",
     # port: 587,
     # domain: "mylovelyplace.herokuapp.com",
